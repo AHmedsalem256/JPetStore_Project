dedb50f44a7b5912f9cf7e219f4897220b742dd1
= <=>
2025-10-20 00:57:14 +0300

    Modify ShoppingCart Module

--- src/main/java/Pages/ShoppingCartPage.java
+++ src/main/java/Pages/ShoppingCartPage.java
@@ -1,6 +1,7 @@
 package Pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class ShoppingCartPage extends BasePage{
@@ -10,8 +11,33 @@ public class ShoppingCartPage extends BasePage{
     }
 
 
-    /*Important btns's*/
-    @FindBy()
+    @FindBy(partialLinkText = "Add to Cart")
+    WebElement AddCartBtn;
+
+    @FindBy(xpath = "//*[@id=\"jpetstore-content\"]/div[2]/form/table/tbody/tr/td[5]/input")
+    WebElement QuantityField;
+
+    @FindBy(className = "btn btn-primary btn-sm")
+    WebElement UpdateCartBtn;
+
+    @FindBy(xpath = "//td[contains(text(), '$')]")
+    WebElement PriceGetter;
+
+
+    public void GetPrice(){
+        String Price = PriceGetter.getText();
+        Price = Price.replace("$"," ");
+        double price = Double.parseDouble(Price);
+        System.out.println(price);
+    }
+
+
+    public void AddQuantity_UpdateCart(){
+        AddCartBtn.click();
+        QuantityField.clear();
+        QuantityField.sendKeys("15");
+        UpdateCartBtn.click();
+    }
 
 
 
--- src/test/java/Tests/ShoppingCartTest.java
+++ src/test/java/Tests/ShoppingCartTest.java
@@ -1,30 +1,48 @@
 package Tests;
 
+import Pages.*;
 import org.testng.annotations.Test;
 
 public class ShoppingCartTest extends BaseTest {
 
-    @Test
-    public void TC_SC_01(){
-        System.out.println("Hello World");
-    }
+
+    ProductDetailPage Prod_obj ;
+    AuthenticationPage Auth_Obj ;
+    CatalogPage C_Obj;
+    ShoppingCartPage S_Obj;
 
     @Test
+    public void TC_SC_01(){
+
+        Auth_Obj = new AuthenticationPage(driver);
+        Prod_obj = new ProductDetailPage(driver);
+        C_Obj = new CatalogPage(driver);
+        S_Obj = new ShoppingCartPage(driver);
+
+
+        Auth_Obj.Auth_PerformFullLogin();
+        C_Obj.ClickDogsLink();
+        Prod_obj.SelectSpecific_Product_Item();
+        S_Obj.AddQuantity_UpdateCart();
+
+    }
+
+    @Test(enabled = false)
     public void TC_SC_02(){
 
     }
 
-    @Test
+    @Test(enabled = false)
     public void TC_SC_03(){
 
     }
 
-    @Test
+    @Test(enabled = false)
     public void TC_SC_04(){
 
     }
 
-    @Test
+    @Test(enabled = false)
     public void TC_SC_05(){
 
     }
